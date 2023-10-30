ca358f8b0e477cdf3daefd937ef5ee67c52079f7
Sunil Shrestha <shrest464760+github@gmail.com>
2023-10-31 00:33:40 +0545

    Configured tailwindcss in the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-semibold text-center">Welcome to SvelteKit</h1>
+<section class="prose">
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</section>
