ed29d086e6e924dad33d3d9043850c92c1838c21
Sunil Shrestha <shrest464760+github@gmail.com>
2023-10-31 01:23:24 +0545

    Added daisy ui styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <h1 class="text-3xl font-semibold text-center">Welcome to SvelteKit</h1>
 <section class="prose">
+	<h3>Welcome</h3>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </section>
+
