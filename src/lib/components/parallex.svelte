5b7a33467ccba3a7cca7cdb623025d53b3cae406
Sunil Shrestha <shrest464760+github@gmail.com>
2023-11-26 23:15:49 +0545

    Added the ui components for headers

---
+++ src/lib/components/parallex.svelte
@@ -0,0 +1,10 @@
+<section class="pt-40 pb-32 relative">
+	<div
+		class="absolute w-full h-[1000px] top-0 left-0 bg-cover bg-center bg-no-repeat opacity-80 bg-fixed"
+		style="background-image:url(https://d1pnnwteuly8z3.cloudfront.net/images/dafc1e05-b0e8-4c6d-b375-4a62333bbd5a/71fe09e2-9320-4f61-8eca-067243e2ef68.jpeg)"
+	/>
+	<h1 class="text-white display-2 text-center relative">Parallax Header</h1>
+</section>
+<section class="h-[500px]">
+	<h1 class="text-3xl font-semibold text-center">Welcome to SvelteKit</h1>
+</section>
