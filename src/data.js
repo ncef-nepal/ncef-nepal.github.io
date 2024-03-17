1c061ecbdd746e56db295ef565b80d07a81fe539
Sunil Shrestha <35072109+Shrest4647@users.noreply.github.com>
2024-03-17 10:33:23 +0545

    Adding gallery component

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const galleryImages = [
+    {
+      title: '5th Monthly Meetup',
+      description: '5th Monthly Meetup at <em>Patan Church</em>',
+      date: '2024-03-15',
+      images: [
+        'https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg',
+        'https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg',
+        'https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg',
+        'https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg',
+        'https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg',
+        'https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg',
+        'https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg',
+      ],
+    },
+  
+    {
+      title: '4th Monthly Meetup',
+      description: '4th Monthly Meetup at <em>Patan Church</em>',
+      date: '2024-03-15',
+      images: [
+        'https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg',
+        'https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg',
+        'https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg',
+        'https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg',
+        'https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg',
+        'https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg',
+      ],
+    },
+    {
+      title: '3rd Monthly Meetup',
+      description: '3rd Monthly Meetup at <em>Patan Church</em>',
+      date: '2024-03-15',
+      images: [
+        'https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg',
+        'https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg',
+        'https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg',
+        'https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg',
+        'https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg',
+      ],
+    },
+    
+  ];
+  
